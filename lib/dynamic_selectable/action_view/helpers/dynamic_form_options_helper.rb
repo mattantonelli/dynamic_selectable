27b15f53c8cd8763ab2e8d2f3340fb3be8525ef0
Matt Antonelli <mattr.antonelli@gmail.com>
2015-11-02 21:05:43 -0500

    Making the dynamic helper perform more naturally
    * More closely matches the format/behavior of the standard collection_select
      * `options` and `html_options` arguments now have default arguments of `{}`
        * Fixes bug where data options would fail to merge into a
          non-existent html_options hash
      * First two arguments are now `object` and `method`

--- lib/dynamic_selectable/action_view/helpers/dynamic_form_options_helper.rb
+++ lib/dynamic_selectable/action_view/helpers/dynamic_form_options_helper.rb
@@ -2,11 +2,11 @@ module DynamicSelectable
   module ActionView
     module Helpers
       module DynamicFormOptionsHelper
-        def dynamic_collection_select(current, select_parent, select_child, collection, value_method, text_method,
-                                      options, html_options)
-          select_url    = "dynamic_selectable_#{select_child.to_s.pluralize}_path"
-          parent_id     = ":#{select_parent}_id"
-          select_target = "##{current}_#{select_child}_id"
+        def dynamic_collection_select(object, method, dynamic_model, collection, value_method, text_method,
+                                      options = {}, html_options = {})
+          select_url    = "dynamic_selectable_#{dynamic_model.to_s.pluralize}_path"
+          parent_id     = ":#{method}"
+          select_target = "##{object}_#{dynamic_model}_id"
 
           data_options = { data: { dynamic_selectable_url: send(select_url, parent_id),
                                    dynamic_selectable_target: select_target } }
