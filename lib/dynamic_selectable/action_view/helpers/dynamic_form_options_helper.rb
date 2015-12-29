a5214f36e8884665cdca7c0b8fa979a21164518a
Matt Antonelli <mattr.antonelli@gmail.com>
2015-12-29 19:42:04 +0000

    Made the generated routes more RESTful
    * Route information updated as follows:
      * dynamic_selectable_models → dynamic_selectable_make_models
      * /dynamic_selectable/:make_id/models → /dynamic_selectable/makes/:make_id/models
      * dynamic_selectable/models#index → dynamic_selectable/make_models#index
    * Unfortunately, this will require all content generated using
      dynamic_selectable:select in prior versions to be regenerated

--- lib/dynamic_selectable/action_view/helpers/dynamic_form_options_helper.rb
+++ lib/dynamic_selectable/action_view/helpers/dynamic_form_options_helper.rb
@@ -4,7 +4,7 @@ module DynamicSelectable
       module DynamicFormOptionsHelper
         def dynamic_collection_select(object, method, dynamic_model, collection, value_method, text_method,
                                       options = {}, html_options = {})
-          select_url    = "dynamic_selectable_#{dynamic_model.to_s.pluralize}_path"
+          select_url    = "dynamic_selectable_#{method.to_s.sub(/_id$/, '')}_#{dynamic_model.to_s.pluralize}_path"
           parent_id     = ":#{method}"
           select_target = "##{object}_#{dynamic_model}_id"
 
